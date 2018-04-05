d47d6d92ecd51841df033c5edd69bcbca381c0be
Jessica C <jgcreiglow@gmail.com>
2018-04-05 09:41:42 -0400

    prep for deployment

---
+++ server.js
@@ -0,0 +1,96 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/wapo", {
+    useMongoClient: true
+});
+
+
+// Routes
+
+app.get("/scrape", function (req, res) {
+    // First, we grab the body of the html with request
+    axios.get("http://www.washingtonpost.com/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("div.pb-root").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+            .then(function (dbArticle) {
+                // View the added result in the console
+                console.log(dbArticle);
+            })
+            .catch(function (err) {
+                // If an error occurred, send it to the client
+                return res.json(err);
+            });
+        });
+    });
+});
+
+
+app.get("/", function (req, res) {
+    res.render("");
+});
+app.get("/home", function (req, res) {
+    res.render("home");
+});
+
+app.get("/results", function (req, res) {
+    res.render("results");
+});
+
+app.get("/saved", function (req, res) {
+    res.render("saved");
+});
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
